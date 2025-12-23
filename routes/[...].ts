ae2f845d583eef37480120126f01f5cd40f1c9a6
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2025-12-23 13:03:43 +0200

    fix 404

--- routes/[...].ts
+++
@@ -1,8 +0,0 @@
-import { defineEventHandler, setResponseStatus } from "h3";
-
-export default defineEventHandler((event) => {
-    setResponseStatus(event, 404);
-    return {
-        message: `Route ${event.path} not found`
-    };
-});
