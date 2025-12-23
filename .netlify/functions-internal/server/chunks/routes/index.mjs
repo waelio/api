74741811e98de20ab55723c555680b6bdbba88d6
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2025-12-23 13:36:53 +0200

    fix 404 - 3

---
+++ .netlify/functions-internal/server/chunks/routes/index.mjs
@@ -0,0 +1,10 @@
+import { e as eventHandler } from '../_/h3.mjs';
+import 'rou3';
+import 'srvx';
+
+const index = eventHandler(() => {
+  return "<h1>Welcome to the API</h1>";
+});
+
+export { index as default };
+//# sourceMappingURL=index.mjs.map
---
+++ .netlify/functions-internal/server/chunks/routes/quran.mjs
@@ -0,0 +1,10 @@
+import { e as eventHandler } from '../_/h3.mjs';
+import 'rou3';
+import 'srvx';
+
+const quran = eventHandler(() => {
+  return { status: "OK", message: "Quran route works!" };
+});
+
+export { quran as default };
+//# sourceMappingURL=quran.mjs.map
